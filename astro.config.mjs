30204f201a27f7f8c7d2f7a42f25773546b59a8d
CESAR GONZALEZ SALAZAR <123420180+Zer0M4n@users.noreply.github.com>
2024-10-20 17:11:46 -0700

    beta 0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://zer0m4n.github.io',
-  base: 'portfolio-dev',
+  base: 'my-repo',
 });
